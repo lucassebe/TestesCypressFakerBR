657aab54c54995412fd7ca889ef6fda9182667d3
Lucas Sebe <lucas.sebe@cerusbank.com.br>
2022-10-31 11:36:37 -0300

    ajustando cadastro

--- cypress/e2e/testes IB/apiemployerscreate.cy.js
+++ cypress/e2e/testes IB/apiemployerscreate.cy.js
@@ -3,24 +3,42 @@ beforeEach(() => {
 });
 
 describe('API Employee Create - Test', () => {
+    const faker = require('faker-br');
+    var randomcpf = faker.br.cpf();
+    var randomname = faker.name.firstName('male') + ' ' + faker.name.lastName('male');
+    var randommothername = faker.name.firstName('female') + ' ' + faker.name.lastName('female');
+    var ranndomemail = faker.internet.email('lucas', 'sebe');
+    var randomjobTitle = faker.name.jobTitle();
+    var randominternalcode = faker.random.alpha(10);
+    var randomphonenumber = faker.phone.phoneNumber('859########');
+
     it('API Employee Create - Test', () => {
+        cy.api({
+            auth: { bearer: window.localStorage.token },
+            method: 'POST',
+            url: '  https://cerusbank-portal-api-dev.uc.r.appspot.com/employees/valid-employee',
+            body: {
+                "company_id": "924b154e62d645c09edc08dad76b03b8",
+                "employee_identifier": randomcpf
+            }
+        })
         cy.api({
             auth: { bearer: window.localStorage.token },
             method: 'POST',
             url: '  https://cerusbank-portal-api-dev.uc.r.appspot.com/employees/create',
             body: {
-                "name": "teste gustavo",
-                "cpf": "85970583073",
-                "email": "lucasssss@gmail.com",
-                "cellphone": "85999988888",
-                "gender": 0,
+                "name": randomname,
+                "cpf": randomcpf,
+                "email": ranndomemail,
+                "cellphone": randomphonenumber,
+                "gender": "0",
                 "birth_date": "1997-12-22T02:00:00.000Z",
-                "responsibility": "",
-                "admission_date": "2022-09-12T14:38:07.198Z",
-                "internal_code": "64645132123",
+                "responsibility": randomjobTitle,
+                "admission_date": "2022-10-31T14:02:19.792Z",
+                "internal_code": randominternalcode,
                 "nationality": 1,
                 "company_id": "924b154e62d645c09edc08dad76b03b8",
-                "mother_full_name": "maemae"
+                "mother_full_name": randommothername
             },
         }).should((res) => {
             expect(res.status).to.eq(200)
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,3 +1,4 @@
 import 'cypress-plugin-api'
 import './commands'
 import "cypress-localstorage-commands"
+import 'faker-br'
