14d0bc209fcb54fdc876585a967e81d8ee58bf20
Lucas Sebe <lucas.sebe@cerusbank.com.br>
2022-11-01 10:07:04 -0300

    primeiro commit

--- cypress/support/cadastrodefuncionarios_commands.js
+++
@@ -1,5 +0,0 @@
-Cypress.Commands.add('cadastrodefuncionarios',
-    (name, email, cellphone, gender, birth_date, responsibility, admission_date, internal_code, nationality, company_id, mother_full_name) => {
-        const faker = require('faker')
-
-    })
